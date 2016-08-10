da6f7774f9562ffd031833d7dafc3f23aa9d5c1e
Thanassis Tsiodras <ttsiodras@gmail.com>
2016-08-10 10:09:03 +0200

    Amazing algorithmic optimization.

--- contrib/HexSpeak-C++/src/hexspeak.cpp
+++ contrib/HexSpeak-C++/src/hexspeak.cpp
@@ -27,15 +27,56 @@ list<string> getGoodWords(const string& filename)
     return goodWords;
 }
 
+// mian2zi3 sent me this amazing alternate implementation...
+// He realized that the words themselves don't matter - only their lengths.
+// So instead of a vector of pointers to the words, wordsByLength
+// contains just counters - e.g. wordsByLength[3] = 18.
+// (there are 18 words of length 3 - "bed", "bee", etc).
+// He can then handle all 18 such words in one single stroke,
+// by multiplying the result of solve after using any one of them
+// by 18! (that's what the 't*solve' does below...)
+//
+// Brilliant - and of course, 1000s of times faster than recursing
+// on the words themselves.
+//
+// As ever, algorithmic improvements can be far more important 
+// than language optimizations :-)
+//
+// Kudos, mian2zi3 !
+//
+// Below, the check verifying the results:
+//
+//    $ for i in {3..14} ; do ../hexspeak.py $i abcdef ../words  ; done | grep -v ^$
+//
+//    18 in   0.023 ms
+//    56 in   0.061 ms
+//    48 in   0.080 ms
+//    335 in   0.175 ms
+//    1667 in   0.654 ms
+//    2834 in   2.868 ms
+//    7688 in   6.172 ms
+//    40992 in  17.437 ms
+//    103684 in  68.678 ms
+//    222138 in 198.328 ms
+//    946764 in 617.432 ms
+//    3020796 in 1961.062 ms
+//    
+//    $ for i in {3..14} ; do ./bin.debug/hexspeak $i abcdef ../words  ; done | grep -v ^$
+//
+//    18 in 0.001959 ms.
+//    56 in 0.002233 ms.
+//    48 in 0.00229 ms.
+//    335 in 0.003186 ms.
+//    1667 in 0.005968 ms.
+//    2834 in 0.005749 ms.
+//    7688 in 0.005978 ms.
+//    40992 in 0.035903 ms.
+//    103684 in 0.018924 ms.
+//    222138 in 0.022032 ms.
+//    946764 in 0.056376 ms.
+//    3020796 in 0.07887 ms.
+//
 int solve(
-    // Use dirty tricks :-) - intern strings (i.e. use their
-    // insides (const char*) instead of actual 'wrapper'
-    // string instances, use vectors instead of
-    // lists for much better cache coherency (so that
-    // algorithm::find ends up searching tiny cache lines!), etc.
-    //
-    // In plain words: BEEP BEEP (Java falls in pieces :-)
-    //
     vector<int>& wordsByLength,
     int targetLength,
     int phraseLength)
@@ -45,10 +86,9 @@ int solve(
         if (phraseLength+i == targetLength) {
             counter += wordsByLength[i];
         } else {
-            int t = wordsByLength[i];
-            --wordsByLength[i];
+            int t = wordsByLength[i]--;
             counter += t * solve(wordsByLength, targetLength, phraseLength+i);
-            wordsByLength[i] = t;
+            wordsByLength[i]++;
         }
     }
     return counter;
@@ -64,11 +104,9 @@ int main(int argc, char *argv[])
     vector<int> wordsByLength(128);
     for(const auto& word: goodWords)
         wordsByLength[word.length()]++;
-    for(int i=0; i<10; i++) {
-        auto startTime = chrono::steady_clock::now();
-        counter = solve(wordsByLength, targetLength, 0);
-        auto endTime = chrono::steady_clock::now();
-        cout << counter << " in " << chrono::duration<double, milli>(endTime-startTime).count()  << " ms.\n";
-    }
+    auto startTime = chrono::steady_clock::now();
+    counter = solve(wordsByLength, targetLength, 0);
+    auto endTime = chrono::steady_clock::now();
+    cout << counter << " in " << chrono::duration<double, milli>(endTime-startTime).count()  << " ms.\n";
     cout << endl;
 }
